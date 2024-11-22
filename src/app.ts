72656c5813fb67fbbabc1db2d288691cf4e75e9f
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-11-22 10:24:11 +0600

    🎉Application setup add

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+//Parser calling
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//CORS
+app.use(cors());
+
+//health route
+app.get("/health", (_req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Server Health are fine now! ",
+  });
+});
+export default app;
